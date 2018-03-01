bb14df32b610192994274f976691bec300009d28
Ethiraric <floriansabourin@live.fr>
2018-03-01 15:19:57 +0100

    Add LinkedList with insertion methods.

---
+++ src/LinkedList.java
@@ -0,0 +1,86 @@
+public final class LinkedList<T> {
+    class Node {
+        public T value;
+        public Node next;
+
+        public Node(T value, Node next) {
+            this.value = value;
+            this.next = next;
+        }
+
+        public Node(T value) {
+            this(value, null);
+        }
+
+        public boolean hasNext() {
+            return this.next != null;
+        }
+    }
+
+    public class Iterator {
+        public Node node;
+
+        public Iterator(Node node) {
+            this.node = node;
+        }
+
+        public void advance(int pos) {
+            while (pos != 0) {
+                this.node = this.node.next;
+                --pos;
+            }
+        }
+    }
+
+    public LinkedList() {
+        this.head = null;
+        this.tail = null;
+        this.size = 0;
+    }
+
+    public Iterator begin() {
+        return new Iterator(this.head);
+    }
+
+    public Iterator end() {
+        return new Iterator(this.tail);
+    }
+
+    public void addHead(T value) {
+        Node new_head = new Node(value, head);
+        this.head = new_head;
+        ++this.size;
+    }
+
+    public void addTail(T value) {
+        Node new_tail = new Node(value);
+        this.tail.next = new_tail;
+        this.tail = new_tail;
+        ++this.size;
+    }
+
+    public void addAtPosition(T value, int pos) throws IndexOutOfBoundsException {
+        if (pos > this.size || pos < 0)
+            throw new IndexOutOfBoundsException();
+        else if (pos == 0)
+            this.addHead(value);
+        else if (pos == this.size)
+            this.addTail(value);
+        else
+            this.addInTheMiddle(value, pos);
+    }
+
+    private void addInTheMiddle(T value, int pos) {
+        Iterator it_before_insertion = this.begin();
+        Node new_node = new Node(value);
+        it_before_insertion.advance(pos - 1);
+        Node node_before_insertion = it_before_insertion.node;
+        new_node.next = node_before_insertion.next;
+        node_before_insertion.next = new_node;
+        ++this.size;
+    }
+
+    private Node head;
+    private Node tail;
+    private int size;
+}
