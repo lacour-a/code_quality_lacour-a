283d029ad5197a101ecad1c30f4ceabb37e2714e
Ethiraric <floriansabourin@live.fr>
2018-03-03 14:31:42 +0100

    Add TableDisplayer with tests.

---
+++ src/main/java/TableDisplayer.java
@@ -0,0 +1,116 @@
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class TableDisplayer {
+  private static final int MIN_DAY_COLUMN_WIDTH = 6;
+  private int numberOfDays;
+  private List<Forecast> forecasts;
+  int providerColumnWidth;
+  private List<Integer> daysColumnWidths;
+  private StringBuilder output;
+
+  public static String forecastsToString(int numberOfDays, List<Forecast> forecasts) {
+    TableDisplayer displayer = new TableDisplayer(numberOfDays, forecasts);
+    return displayer.convertToString();
+  }
+
+  private TableDisplayer(int numberOfDays, List<Forecast> forecasts) {
+    this.numberOfDays = numberOfDays;
+    this.forecasts = forecasts;
+    this.output = new StringBuilder();
+  }
+
+  private String convertToString() {
+    this.computeNameColumnWidth();
+    this.computeDaysColumnWidths();
+
+    this.printHorizontalBorder();
+    this.printHeaderRow();
+    this.printHorizontalBorder();
+    for (Forecast forecast : this.forecasts) {
+      this.printForecastRow(forecast);
+      this.printHorizontalBorder();
+    }
+    return this.output.toString();
+  }
+
+  private void computeNameColumnWidth() {
+    this.providerColumnWidth = this.forecasts.stream()
+            .map(forecast -> forecast.provider.length())
+            .max(Comparator.naturalOrder())
+            .get();
+  }
+
+  private static int getTemperatureWidth(DayForecast dayForecast) {
+    return getTemperatureString(dayForecast).length();
+  }
+
+  private Integer getMaxTemperatureColumnWidthForDay(int dayNumber) {
+    return this.forecasts.stream()
+            .map(forecast -> forecast.daysForecast.get(dayNumber))
+            .map(TableDisplayer::getTemperatureWidth)
+            .max(Comparator.naturalOrder())
+            .get();
+  }
+
+  private void computeDaysColumnWidths() {
+    this.daysColumnWidths = IntStream.range(0, this.numberOfDays)
+            .mapToObj(dayNumber -> this.getMaxTemperatureColumnWidthForDay(dayNumber))
+            .map(width -> Integer.max(width, MIN_DAY_COLUMN_WIDTH))
+            .collect(Collectors.toList());
+  }
+
+  private void printHorizontalBorder() {
+    this.output.append(String.format("+-%s-+", repeatCharToString('-', this.providerColumnWidth)));
+    for (Integer dayColumnWidth : this.daysColumnWidths) {
+      this.output.append(String.format("-%s-+", repeatCharToString('-', dayColumnWidth)));
+    }
+    this.output.append('\n');
+  }
+
+  private void printHeaderRow() {
+    this.output.append(String.format("| %s |", repeatCharToString(' ', this.providerColumnWidth)));
+    for (int dayNumber = 0; dayNumber < this.numberOfDays; ++dayNumber) {
+      String cellContents = String.format("J+%d", dayNumber);
+      this.output.append(String.format(" %s%s |",
+              cellContents,
+              repeatCharToString(' ',
+                      this.daysColumnWidths.get(dayNumber) - cellContents.length())));
+    }
+    this.output.append('\n');
+  }
+
+  private void printForecastRow(Forecast forecast) {
+    this.output.append(String.format("| %s%s |",
+            forecast.provider,
+            repeatCharToString(' ', this.providerColumnWidth - forecast.provider.length())));
+    for (int dayNumber = 0; dayNumber < this.numberOfDays; ++dayNumber) {
+      this.printForecastTemperatureCell(
+              forecast.daysForecast.get(dayNumber),
+              this.daysColumnWidths.get(dayNumber));
+    }
+    this.output.append('\n');
+  }
+
+  private void printForecastTemperatureCell(DayForecast dayForecast, int width) {
+    String cellContents = getTemperatureString(dayForecast);
+    this.output.append(String.format(" %s%s |",
+            cellContents,
+            repeatCharToString(' ', width - cellContents.length())));
+  }
+
+  private static String getTemperatureString(DayForecast dayForecast) {
+    return String.format("%d°-%d°",
+            dayForecast.minTemp.intValue(),
+            dayForecast.maxTemp.intValue());
+  }
+
+  private static String repeatCharToString(char c, int repetitions) {
+    char[] repeat = new char[repetitions];
+    Arrays.fill(repeat, c);
+    return new String(repeat);
+  }
+}
---
+++ src/test/java/TableDisplayerTest.java
@@ -0,0 +1,53 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+public class TableDisplayerTest {
+  List<Forecast> forecasts;
+
+  @Before
+  public void setupForecast() {
+    this.forecasts = new ArrayList<>();
+    Forecast forecast = new Forecast();
+    forecast.provider = "MyProvider";
+    forecast.daysForecast.add(new DayForecast(1.d, 2.d));
+    forecast.daysForecast.add(new DayForecast(2.d, 3.d));
+    forecast.daysForecast.add(new DayForecast(3.d, 4.d));
+    forecast.daysForecast.add(new DayForecast(4.d, 5.d));
+    forecast.daysForecast.add(new DayForecast(10.d, 20.d));
+    this.forecasts.add(forecast);
+  }
+
+  @Test
+  public void testForecastsToStringOneDay() {
+    StringBuilder expectedBuilder = new StringBuilder();
+    expectedBuilder.append("+------------+--------+\n");
+    expectedBuilder.append("|            | J+0    |\n");
+    expectedBuilder.append("+------------+--------+\n");
+    expectedBuilder.append("| MyProvider | 1°-2°  |\n");
+    expectedBuilder.append("+------------+--------+\n");
+    String result = TableDisplayer.forecastsToString(1, this.forecasts);
+    assertEquals(expectedBuilder.toString(), result);
+  }
+
+  @Test
+  public void testForecastsToStringTwoDays() {
+    StringBuilder expectedBuilder = new StringBuilder();
+    expectedBuilder.append("+------------+--------+--------+\n");
+    expectedBuilder.append("|            | J+0    | J+1    |\n");
+    expectedBuilder.append("+------------+--------+--------+\n");
+    expectedBuilder.append("| MyProvider | 1°-2°  | 2°-3°  |\n");
+    expectedBuilder.append("+------------+--------+--------+\n");
+    String result = TableDisplayer.forecastsToString(2, this.forecasts);
+    assertEquals(expectedBuilder.toString(), result);
+  }
+
+  @After
+  public void teardownForecast() {
+  }
+}
